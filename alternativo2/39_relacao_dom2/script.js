799810e2836a6c5da6cbc1d71b10a624112bf45c
Amando Barbosa <129077742+amandobarbosa@users.noreply.github.com>
2023-12-05 17:17:36 -0300

    parentnode

---
+++ alternativo2/39_relacao_dom2/script.js
@@ -0,0 +1,26 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+
+const c1_2 = document.getElementById('c1_2')
+
+console.log(c1_2.parentNode.parentNode.children[4])
+/*console.log(caixa1.hasChildNodes())
+console.log(btn_c[0].hasChildNodes())
+
+if(btn_c[0].children.length > 0){
+    console.log('tem filhos')
+}else{
+    console.log('não tem filhos')
+}
+
+console.log(caixa1.firstElementChild)
+
+caixa1.children[1].innerHTML = 'oi'*/
+
+
+//console.log(caixa1.children[caixa1.children.length-1])
+
+/*console.log(caixa1.firstElementChild)
+console.log(caixa1.lastElementChild)
+console.log(caixa1.children)*/
+
